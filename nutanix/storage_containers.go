1ea50cb4a42331cbe8e22a176ec83c3e5b938e02
crlin tsai <crlin.tsai@msa.hinet.net>
2022-08-11 15:01:18 +0800

    Change Storage container ID to UUID

--- nutanix/storage_containers.go
+++ nutanix/storage_containers.go
@@ -4,7 +4,7 @@ package nutanix
 import "encoding/json"
 
 type StorageEntity struct {
-	Id				string  `json:"id"`
+	Id				string  `json:"storage_container_uuid"`
 	Name				string  `json:"name"`
 	MaxCapacity			uint64	`json:"max_capacity"`
 	Stats				map[string]string	`json:"stats"`
